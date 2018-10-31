a57c6eefba77d540724df79f96606e2921e89892
Chris Cowled <chris.cowled@csiro.au>
2018-10-31 14:52:22 +0000

    rebuilt docs

--- aliseq.sh
+++ aliseq.sh
@@ -2,10 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-snakemake --cluster-config $DIR/cluster.json \
-          --jobscript $DIR/custombash.sh \
+snakemake --cluster-config $DIR/scripts/cluster.json \
+          --jobscript $DIR/scripts/custombash.sh \
           --use-conda \
           --cluster "qsub -cwd -pe sharedmem {cluster.core} -l h_rt={cluster.time} -l h_vmem={cluster.vmem}" \
           --jobs 1000 \
-          --snakefile $DIR/workflow.snk
+          --snakefile $DIR/scripts/workflow.snk
 
